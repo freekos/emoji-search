eda5e67e00a7ea5026548a699f99d94be56e9587
Freekos <maks.mafia00@mail.ru>
2022-05-13 22:18:52 +0300

    Did Title

---
+++ src/components/Title/Title.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import "./style.scss"
+
+const Title = () => {
+
+    return (
+        <div className='title'>
+            <p>Emoji Search</p>
+        </div>
+    )
+}
+
+export default Title;
