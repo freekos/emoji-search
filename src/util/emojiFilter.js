6963f8c41074ef6bde1c2ae8dc47a2e676c05b3d
Freekos <maks.mafia00@mail.ru>
2022-05-13 22:23:44 +0300

    Did a logic for search from input

---
+++ src/util/emojiFilter.js
@@ -0,0 +1,15 @@
+import emojiList from "../../emojiList.json";
+
+const emojiFilter = (content, maxResults) => {
+    return emojiList.filter(emoji => {
+        if(emoji.title.toLowerCase().includes(content.toLowerCase())) {
+            return true;
+        }
+        if(emoji.keywords.includes(content)) {
+            return true;
+        }
+        return false;
+    }).slice(0, maxResults)
+}
+
+export default emojiFilter;
